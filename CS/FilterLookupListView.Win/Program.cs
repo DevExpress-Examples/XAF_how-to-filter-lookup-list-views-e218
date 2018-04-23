8fcc5597ec65cdd4e1f3cf6e79d39655cd6f4549
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilterLookupListView.Win/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+
+using FilterLookupListView.Module;
+
+namespace FilterLookupListView.Win {
+   static class Program {
+      /// <summary>
+      /// The main entry point for the application.
+      /// </summary>
+      [STAThread]
+      static void Main() {
+#if EASYTEST
+			DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register();
+#endif
+
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
+         FilterLookupListViewWindowsFormsApplication winApplication = new FilterLookupListViewWindowsFormsApplication();
+         winApplication.ConnectionString = CodeCentralExampleInMemoryDataStoreProvider.ConnectionString;
+#if EASYTEST
+			if(ConfigurationManager.ConnectionStrings["EasyTestConnectionString"] != null) {
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings["EasyTestConnectionString"].ConnectionString;
+			}
+#endif
+         if (ConfigurationManager.ConnectionStrings["ConnectionString"] != null) {
+            winApplication.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+         }
+         try {
+            // Uncomment this line when using the Middle Tier application server:
+            // new DevExpress.ExpressApp.MiddleTier.MiddleTierClientApplicationConfigurator(winApplication);
+            winApplication.Setup();
+            winApplication.Start();
+         }
+         catch (Exception e) {
+            winApplication.HandleException(e);
+         }
+      }
+   }
+}
---
+++ VB/FilterLookupListView.Module.Web/WebModule.vb
@@ -0,0 +1,15 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace FilterLookupListView.Module.Web
+   <ToolboxItemFilter("Xaf.Platform.Web")> _
+   Public NotInheritable Partial Class FilterLookupListViewAspNetModule
+	   Inherits ModuleBase
+	  Public Sub New()
+		 InitializeComponent()
+	  End Sub
+   End Class
+End Namespace
