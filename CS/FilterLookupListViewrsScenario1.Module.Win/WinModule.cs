df38f5ff7875b19d24dc38aa8a00bc7b397371a2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilterLookupListViewrsScenario1.Module.Win/WinModule.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace FilterLookupListViewrsScenario1.Module.Win
+{
+    [ToolboxItemFilter("Xaf.Platform.Win")]
+    public sealed partial class FilterLookupListViewrsScenario1WindowsFormsModule : ModuleBase
+    {
+        public FilterLookupListViewrsScenario1WindowsFormsModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
